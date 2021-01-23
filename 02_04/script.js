55fba26c511b7c197975ef572ad8d7c593790bd7
JamesVanWaza <jgacuca@gmail.com>
2021-01-23 13:32:51 -0500

    Beautified

--- 02_04/script.js
+++ 02_04/script.js
@@ -4,16 +4,15 @@
 import backpack from "./backpack.js";
 
 const markup = (backpack) => {
-  return `
+    return `
   <div>
     <h3>${backpack.name}</h3>
     <ul>
       <li>Volume: ${backpack.volume}</li>
       <li>Color: ${backpack.color}</li>
       <li>Number of pockets: ${backpack.pocketNum}</li>
-      <li>Strap lengths: L: ${backpack.strapLength.left}, R: ${
-    backpack.strapLength.right
-  } </li>
+      <li>Strap lengths: L: ${backpack.strapLength.left}, R: ${backpack.strapLength.right
+    } </li>
       <li>Top lid: ${backpack.lidOpen ? "Open" : "Closed"}</li>
     </ul>
   </div>
