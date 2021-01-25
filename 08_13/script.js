e7a442d0cc9a911afbe051982bf707022f815fc4
JamesVanWaza <jgacuca@gmail.com>
2021-01-24 22:02:23 -0500

    Beautified

--- 08_13/script.js
+++ 08_13/script.js
@@ -6,15 +6,15 @@
 import Backpack from "./Backpack.js";
 
 const everydayPack = new Backpack(
-  "Everyday Backpack",
-  30,
-  "grey",
-  15,
-  26,
-  26,
-  false,
-  "December 5, 2018 15:00:00 PST",
-  "../assets/images/everyday.svg"
+    "Everyday Backpack",
+    30,
+    "grey",
+    15,
+    26,
+    26,
+    false,
+    "December 5, 2018 15:00:00 PST",
+    "../assets/images/everyday.svg"
 );
 
 const content = `
@@ -23,25 +23,19 @@ const content = `
     </figure>
     <h1 class="backpack__name">${everydayPack.name}</h1>
     <ul class="backpack__features">
-      <li class="feature backpack__volume">Volume:<span> ${
-        everydayPack.volume
-      }l</span></li>
-      <li class="feature backpack__color">Color:<span> ${
-        everydayPack.color
-      }</span></li>
+      <li class="feature backpack__volume">Volume:<span> ${everydayPack.volume
+  }l</span></li>
+      <li class="feature backpack__color">Color:<span> ${everydayPack.color
+  }</span></li>
       <li class="feature backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-      <li class="feature backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
-      }</span></li>
-      <li class="feature backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
-      } inches</span></li>
-      <li class="feature backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
-      } inches</span></li>
-      <li class="feature backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen ? "open" : "closed"
-      }</span></li>
+      <li class="feature backpack__pockets">Number of pockets:<span> ${everydayPack.pocketNum
+  }</span></li>
+      <li class="feature backpack__strap">Left strap length:<span> ${everydayPack.strapLength.left
+  } inches</span></li>
+      <li class="feature backpack__strap">Right strap length:<span> ${everydayPack.strapLength.right
+  } inches</span></li>
+      <li class="feature backpack__lid">Lid status:<span> ${everydayPack.lidOpen ? "open" : "closed"
+  }</span></li>
     </ul>
   `;
 
@@ -55,23 +49,23 @@ newArticle.innerHTML = content;
 main.append(newArticle);
 
 const usedStatus = () => {
-  let age = everydayPack.backpackAge();
-  let description;
-  if (age >= 30) {
-    if (age >= 365) {
-      if (age >= 1095) {
-        description = "old";
-      } else {
-        description = "used";
-      }
+    let age = everydayPack.backpackAge();
+    let description;
+    if (age >= 30) {
+        if (age >= 365) {
+            if (age >= 1095) {
+                description = "old";
+            } else {
+                description = "used";
+            }
+        } else {
+            description = "lightly used";
+        }
     } else {
-      description = "lightly used";
+        description = "new";
     }
-  } else {
-    description = "new";
-  }
 
-  console.log(`
+    console.log(`
   Age: ${age} days
   Status: ${description}
   `);
