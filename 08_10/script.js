8a5206195395bb376a5a5b264034c45959cd823b
JamesVanWaza <jgacuca@gmail.com>
2021-01-24 21:47:02 -0500

    Removed linebreak

--- 08_10/script.js
+++ 08_10/script.js
@@ -53,5 +53,4 @@ const tipCalculator = (sum, percentage, locale, currency, callback) => {
 };
 
 tipCalculator(29.95, 18, "de-DE", "EUR", printHTML);
-
 tipCalculator(29.95, 18, "nb-NO", "NOK", printHTML);
